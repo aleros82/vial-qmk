236ddb70ba35e1616ede705674ebdf4aef399900
aleros82 <alessandro.rosario.affinito@gmail.com>
2026-04-23 13:49:41 +0200

    Update keymap.c

--- keyboards/axon_labs/vial/keymap.c
+++ keyboards/axon_labs/vial/keymap.c
@@ -1,5 +1,11 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(KC_TRNS, KC_TRNS, KC_TRNS)
+    [0] = LAYOUT(KC_A, KC_B, KC_C)
 };
+
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+    [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) }
+};
+#endif
